5fb4e42acbe9a7f5434933b3e8c9e1082d925274
Bhavani Palla <bhavani.palla@gmail.com>
2021-12-08 09:17:28 +0100

    Added table and functionality

---
+++ js/posts.js
@@ -0,0 +1,31 @@
+const postsElement = document.querySelector(".blog-posts");
+
+async function fetchAllPosts() {
+  console.log("fetching posts");
+  try {
+    let response = await fetch("http://localhost:5000/posts");
+    console.log(response.status);
+    let data = await response.json();
+    let postsHTML = "";
+    for (let post of data) {
+      const postDivElement = document.createElement("div");
+      postDivElement.classList.add("posts");
+
+      postsHTML = `
+            <div class='title'><h3>${post.title}</h3></div>
+            <div class='author'>${post.author}</div>
+            <div class='blog-date'>${post.date}</div>
+            <div class='content'>${post.content}</div>
+            <hr>
+            `;
+
+      postDivElement.innerHTML = postsHTML;
+      postsElement.appendChild(postDivElement);
+    }
+  } catch (error) {}
+}
+
+/**
+ * Fetch all posts
+ */
+fetchAllPosts();
