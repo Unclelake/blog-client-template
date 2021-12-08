d9a731c6b95e41db7bc2d297999d780c08c72db2
Bhavani Palla <bhavani.palla@gmail.com>
2021-12-08 19:44:27 +0100

    Added delete link functionality

--- admin/js/manage-posts.js
+++ admin/js/manage-posts.js
@@ -11,13 +11,15 @@ async function fetchJson() {
 
             const manageDivElement = document.createElement('tr')
             manageDivElement.classList.add('managePost');
+            console.log(post['_id'])
 
             manageHTML = `
             <tr>
             <td>${post.title}</td>
             <td>${post.author}</td>
             <td>${post.date}</td>
-            <td><a href="../admin/update-post.html?id=${post['_id']}">Update</a>|<a herf='#'>Delete</a></td>
+            <td><a href="../admin/update-post.html?id=${post['_id']}">Update</a>|
+            <a class="delete-links" data-id="${post['_id']}" herf='#'>Delete</a></td>
             </tr>
             
             `;
@@ -27,5 +29,30 @@ async function fetchJson() {
     } catch (error) {
         
     }
+    deletePostEvent()
 }
 fetchJson();
+
+function deletePostEvent() {
+
+    let deleteLinks = document.getElementsByClassName('delete-links');
+    console.log(deleteLinks);
+    for (let link of deleteLinks) {
+        link.addEventListener('click', async function (e) {
+            e.preventDefault();
+            // console.log(e.target.dataset.id)
+            try {
+                await fetch("http://localhost:5000/posts/" + e.target.dataset.id,
+                    {
+                        method: 'DELETE'
+                    }
+                );
+                e.target.parentNode.parentNode.remove();
+            
+            } catch (error) {
+                console.log(error);
+            }
+        
+        })
+    }
+}
